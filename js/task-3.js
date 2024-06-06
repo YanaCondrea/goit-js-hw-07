c3fbebab63254c81321a5e4ed3ff1031b45d25bd
YanaCondrea <160020879+YanaCondrea@users.noreply.github.com>
2024-06-06 15:45:41 +0100

    added-task-3.css.js

---
+++ js/task-3.js
@@ -0,0 +1,42 @@
+const inputName = document.querySelector('#name-input');
+const outputName = document.querySelector('#name-output');
+const body = document.querySelector("body");
+const div = document.createElement('div');
+div.appendChild(inputName);
+div.appendChild(outputName.parentElement);
+const p = document.querySelector('p');
+p.insertAdjacentElement('afterend', div);
+inputName.addEventListener("input", (event) => {
+  
+  outputName.textContent = event.currentTarget.value;
+    let trimmedInput = inputName.value.trim();
+    if ( trimmedInput  === '') {
+        outputName.textContent = 'Anonymous';
+    } else {
+        outputName.textContent = trimmedInput;
+    }
+});
+
+console.log(inputName);
+console.log(outputName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
