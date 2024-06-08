38e6a61b0aaf2ca4b40ec05cb967a3c88a4853af
YanaCondrea <160020879+YanaCondrea@users.noreply.github.com>
2024-06-08 18:40:18 +0100

    update.6tasks.css.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,19 +1,20 @@
-const changeColor = document.querySelector('.change-color');
-const areaColor = document.querySelector('body');
-const spanColor = document.querySelector('.color');
-changeColor.addEventListener('click', getRandomHexColor);
-const colors = ['rgba(234, 226, 31, 1)', 'rgba(97, 179, 255, 1)', 'rgba(255, 97, 192, 1)', 'rgba(59, 199, 107, 1)'];
+
+const button = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const span = document.querySelector('.color');
+button.addEventListener('click', handler);
+
+function handler() {
+  let nextColor = getRandomHexColor();
+  body.style.backgroundColor = nextColor;
+  span.textContent = nextColor;
+}
+
 function getRandomHexColor() {
-  let randomIndex = Math.floor(Math.random() * colors.length);
-  let randomColor = `#${Math.floor(Math.random() * 16777215)
+  return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-   let selectedColor = colors[randomIndex];
-   areaColor.style.backgroundColor = selectedColor;
-    spanColor.textContent = selectedColor;
 }
 
 
-console.log(changeColor);
-console.log(areaColor);
-
+console.log(body);
--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,38 @@
+const div = document.querySelector('#controls');
+const input = document.querySelector('input');
+const boxes = document.querySelector('#boxes');
+const buttonCreate = document.querySelector("button[data-create]");
+const buttonDestroy = document.querySelector("button[data-destroy]");
+const divContainer = document.createElement('div');
+divContainer.className += 'container';
+divContainer.appendChild(div);
+divContainer.appendChild(boxes);
+const p = document.querySelector('p');
+p.insertAdjacentElement('afterend', divContainer);
+buttonCreate.addEventListener('click', createBoxes);
+buttonDestroy .addEventListener('click', destroyBoxes);
+function createBoxes(amount) {
+  amount = parseInt(input.value);
+  for (let i = 1; i <= amount; i += 1) {
+    if ([i] >= 1 && [i] <=100) {
+     const box = document.createElement('div')
+        const size = 20 + i * 10;
+       box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      
+       box.style.backgroundColor = getRandomHexColor();
+       boxes.appendChild(box); 
+    }
+    input.value = ''; 
+  } 
+}
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
